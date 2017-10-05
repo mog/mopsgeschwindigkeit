5b441cb6c85c82c79ea692adad9c79333475991b
Mike Gränz <mog@burgeins.de>
2017-10-05 12:32:23 +0200

    Started to lint all SASS files, also adding a lint-sass gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,5 @@ require('require-dir')('./gulp_tasks');
 gulp.task('build', gulpSequence('clean', ['copymanifest', 'copyassets', 'impression', 'sponsors', 'manifest'], ['copydownloads'], ['pug', 'sass', 'browserify'], 'uglify', 'critical'));
 
 gulp.task('default', gulpSequence(['build'], ['browser-sync', 'watch']));
+
+gulp.task('test', gulpSequence(['lint-sass']));
